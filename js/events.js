3d57d0d6ae9b2c6d8b31edafaca13e9457922f79
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-22 19:50:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ function getIt(){
 $('p').on('click', alert('Hey!'))
 }
 function frameIt(){
-$('img').on('load', "border: 2px solid red")
+$('img').on('load', ".tasty")
 }
 function pressIt(){
 $('input').on('keydown', function(key){
