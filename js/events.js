d7271089bda6962bb8420c11129d14d0a7073f0a
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-22 19:47:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,11 @@ function frameIt(){
 $('img').on('load', "border: 2px solid red")
 }
 function pressIt(){
-$('p').on('click', alert('Hey!'))
+$('input').on('keydown', function(key){
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+})
 }
 function submitIt(){
 $('p').on('click', alert('Hey!'))
